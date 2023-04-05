60b9fac03bd257d389a593ae955d34c5456febfd
Markivanarcega01 <arcegamarkivan@gmail.com>
2023-04-05 22:56:55 +0800

    testing

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Grand Casiana Realty</title>
+</head>
+<body>
+    <table style="margin-left:auto;margin-right:auto;margin-top:20%;">
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Project</th>
+            <th>Position</th>
+            <th>Unit type</th>
+            <th>Price</th>
+        </tr>
+        <?php
+        $conn = mysqli_connect("localhost","root","","grandcasianarealty");
+        if ($conn-> connect_error){
+            die ("Connection failed:".$conn-> connect_error);
+        }
+        $sql = "SELECT sales_id, name, project, position, unit_type, price FROM agent a
+        INNER JOIN sales s ON a.agent_id = s.agent_id INNER JOIN project p ON p.project_id = s.project_id";
+        $result = $conn-> query ($sql);
+
+        if ($result-> num_rows > 0){
+            while($row = $result-> fetch_assoc()){
+                echo "<tr><td>".$row["sales_id"]."</td><td>".$row["name"]."</td><td>".$row["project"].
+                "</td><td>".$row["position"]."</td><td>".$row["unit_type"]."</td><td>".$row["price"]."</td></tr>";
+            }
+            echo "</table>";
+        }
+        ?>
+    </table>
+</body>
+</html>
